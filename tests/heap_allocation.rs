dce20358c4a7f983f859b6dd653f6d7c50a8ccc6
Mensah Prince <burnszephyr520@gmail.com>
2026-04-27 13:26:52 +0000

    test: add unit tests for heap allocation including boxes, vectors, and capacity limits

--- tests/heap_allocation.rs
+++ tests/heap_allocation.rs
@@ -35,3 +35,39 @@ fn main(boot_info: &'static BootInfo) -> ! {
 fn panic(info: &PanicInfo) -> ! {
     velora_os::test_panic_handler(info)
 }
+
+
+// A test case for heap allocation
+use alloc::boxed::Box;
+
+#[test_case]
+fn simple_allocation() {
+    let heap_value_1 = Box::new(41);
+    let heap_value_2 = Box::new(13);
+    assert_eq!(*heap_value_1, 41);
+    assert_eq!(*heap_value_2, 13);
+}
+
+// A test case for heap allocation
+use alloc::vec::Vec;
+
+#[test_case]
+fn large_vec() {
+    let n = 1000;
+    let mut vec = Vec::new();
+    for i in 0..n {
+        vec.push(i);
+    }
+    assert_eq!(vec.iter().sum::<u64>(), (n - 1) * n / 2);
+}
+
+// A test case for heap allocation
+use velora_os::allocator::HEAP_SIZE;
+
+#[test_case]
+fn many_boxes() {
+    for i in 0..HEAP_SIZE {
+        let x = Box::new(i);
+        assert_eq!(*x, i);
+    }
+}
